5131c2154302af84a6add23b040523796fa47df7
jyli47 <97953213+jyli47@users.noreply.github.com>
2022-01-29 23:22:14 -0800

    Update test-sketch.js

--- test-sketch.js
+++ test-sketch.js
@@ -1,11 +1,30 @@
 function setup() {
   createCanvas(800, 800);
 }
-var movy=350
+var movy = 0;
+var y = 350;
+var movx = 0;
+var movy=movy+y;
 function draw() {
   background(220);
-  triangle(500,10+movy,500,100+movy,300,100+movy)
-  triangle(100,50+movy,300,50+movy,300,100+movy)
-  rect(300,50+movy,200,50)
+ movx = movx + 1;
+  if (movx>1000) {movx=0; movy=y;}
+  triangle(
+    500 + movx,
+    10 + movy,
+    500 + movx,
+    100 + movy,
+    300 + movx,
+    100 + movy
+  );
+  triangle(
+    100 + movx,
+    50 + movy,
+    300 + movx,
+    50 + movy,
+    300 + movx,
+    100 + movy
+  );
+  rect(300 + movx, 50 + movy, 200, 50);
   fill(204)
 }
